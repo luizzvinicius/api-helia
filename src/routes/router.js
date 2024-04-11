8493e48c2dd5efcecb3266f08fbc78349cc2eec3
Luiz Vinicius <viniciusadr244@gmail.com>
2024-04-11 16:27:22 -0300

    create db in the same ID

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,20 +1,32 @@
 import { Router } from "express";
 import { createHelia, libp2pDefaults } from "helia";
-import { createOrbitDB } from "@orbitdb/core";
+import { createOrbitDB, IPFSAccessController } from "@orbitdb/core";
 import { gossipsub } from "@chainsafe/libp2p-gossipsub";
+import { LevelBlockstore } from 'blockstore-level'
+import { unixfs } from '@helia/unixfs'
 
 const router = Router();
+const encoder = new TextEncoder()
+const decoder = new TextDecoder()
 
-
-
-const initDb = async () => {
-  const libp2pOptions = libp2pDefaults();
+const initIPFSInstance = async () => {
+  const libp2pOptions = libp2pDefaults()
   libp2pOptions.services.pubsub = gossipsub({allowPublishToZeroTopicPeers: true})
-  const helia_instance = await createHelia({ libp2p: libp2pOptions });
-  const orbitdb = await createOrbitDB({ ipfs: helia_instance });
-  const mydb = await orbitdb.open("mydb");
+  // const blockstore = new LevelBlockstore('./ipfs')
+  const helia = await createHelia({libp2p: libp2pOptions})
+  const fs = unixfs(helia)
+  return helia
+}
+
+let databasename = "zdpuB2aYUCnZ7YUBrDkCWpRLQ8ieUbqJEVRZEd5aDhJBDpBqj"
+const initDb = async () => {  
+  const helia_instance = await initIPFSInstance();
+  const orbitdb = await createOrbitDB({ ipfs: helia_instance, directory: `./orbitdb/${databasename}` });
+  const mydb = await orbitdb.open("my-db", { AccessController: IPFSAccessController({ write: ['*']}) });
+  // const mydb = await orbitdb.open(`/orbitdb/${databasename}`, { AccessController: IPFSAccessController({ write: ['*']}) });
   console.log(mydb.address);
-  const hash = await mydb.add("hello world!");
+  // await mydb.add("hello world!");
+  console.log(await mydb.all())
 };
 initDb();
 

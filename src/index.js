d2a7bbd1e9186139a1fcacebcdba4d6526ffdac4
Luiz Vinicius <viniciusadr244@gmail.com>
2024-04-25 11:53:57 -0300

    adapt to requests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import App from "./server.js"
 
-new App().server.listen(3000, () => {
+new App().server.listen(3001, () => {
     console.log("Servidor ligado")
 })
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,60 +1,45 @@
 import { Router } from "express";
-import { createOrbitDB, IPFSAccessController, Documents, IPFSBlockStorage } from "@orbitdb/core";
-import { bootstrap } from '@libp2p/bootstrap'
+import { createOrbitDB, IPFSAccessController } from "@orbitdb/core";
 import { gossipsub } from "@chainsafe/libp2p-gossipsub";
-import { identify } from "@libp2p/identify";
 import { createHelia, libp2pDefaults } from "helia";
 import { LevelBlockstore } from "blockstore-level";
 import { LevelDatastore } from "datastore-level";
-import { createSecp256k1PeerId } from "@libp2p/peer-id-factory";
-import { IDBDatastore } from 'datastore-idb'
-import { peerID } from "../peerId.js";
+import multer from "multer";
+import { unixfs } from "@helia/unixfs";
+import { multiaddr } from "@multiformats/multiaddr";
+import { circuitRelayServer } from "libp2p/circuit-relay";
 
 const router = Router();
+const upload = multer();
+const encoder = new TextEncoder();
 
-const initIPFSInstance = async (dirLevel, dirBlock) => {
+async function initIPFSInstance(dirLevel, dirBlock) {
   const blockstore = new LevelBlockstore(`./src/routes/${dirBlock}`);
-  // const store = new IDBDatastore(`./src/routes/helia_${dirBlock}`);
   const datastore = new LevelDatastore(`./src/routes/${dirLevel}`);
   await blockstore.open();
   await datastore.open();
-  
-  // const id = await createSecp256k1PeerId()
-  // console.log(id);
+
   const libp2pOptions = libp2pDefaults(); // {peerId: id}
   libp2pOptions.services.pubsub = gossipsub({
     allowPublishToZeroTopicPeers: true,
   });
 
-  // const libp2pOptions = {
-  //   services: {
-  //     pubsub: gossipsub({
-  //       allowPublishToZeroPeers: true
-  //     }),
-  //     identify: identify()
-  //   },
-  //   peerDiscovery: [
-  //     bootstrap({
-  //       list: [
-  //         `/ip4/127.0.0.1/tcp/9091/ws/p2p/asdfasdf`
-  //       ]
-  //     })
-  //   ]
-  // }
   const helia = await createHelia({
     libp2p: libp2pOptions,
     blockstore: blockstore,
     datastore: datastore,
   });
-  return helia;
-};
 
-const ipfs1 = await initIPFSInstance("level1", "block1");
-const ipfs2 = await initIPFSInstance("level2", "block2");
+  const fs = unixfs(helia);
+  return { helia: helia, fs: fs };
+}
+
+const { helia: ipfs1, fs: fs1 } = await initIPFSInstance("level1", "block1");
+const { helia: ipfs2, fs: fs2 } = await initIPFSInstance("level2", "block2");
 
 const orbitdb1 = await createOrbitDB({
   ipfs: ipfs1,
-  directory: "./src/routes/orbitdb1"
+  directory: "./src/routes/orbitdb1",
 });
 const orbitdb2 = await createOrbitDB({
   ipfs: ipfs2,
@@ -62,25 +47,62 @@ const orbitdb2 = await createOrbitDB({
 });
 
 const db1 = await orbitdb1.open("my-db", {
-  type: "documents", AccessController: IPFSAccessController({ write: ["*"] }),
+  type: "documents",
+  AccessController: IPFSAccessController({ write: ["*"] }),
 });
 
-const db2 = await orbitdb2.open(db1.address, {
-  type: "documents", AccessController: IPFSAccessController({ write: ["*"] }),
+const db2 = await orbitdb2.open(db1.address);
+
+router.post("/upload", async (req, res) => {
+  // for await (const { key, value } of ipfs1.datastore.query({})) {
+  //   console.log(key.toString());
+  // }
+  const { helia: ipfs1, fs: fs1 } = await initIPFSInstance("level1", "block1");
+  const name = req.body.id
+  const data = req.body.plan
+  console.log(data);
+  const CID = await fs1.addFile({ path: `/${data.title}`, content: encoder.encode(JSON.stringify(data, null, 2)) });
+  await ipfs1.datastore.put(`/${name}/${CID}`, encoder.encode(JSON.stringify(data, null, 2)));
+  await db1.put({ _id: name, content: encoder.encode(JSON.stringify(data, null, 2)), path: `/${data.title}/${CID}`});
+  res.status(200).json({ path: `/${name}`, cid: CID });
 });
 
-// await db1.put({_id: '10', content:'teste documentid'});
-// await db2.del("indexCustom")
+router.get("/all", async (req, res) => {
+  let idsLocal = [];
+  let idsOrbit = [];
+  for await (const { key, value } of ipfs1.datastore.query({})) {
+    idsLocal.push(key.toString());
+  }
+  for await (const record of db2.iterator()) {
+    idsOrbit.push(record);
+  }
+  res.status(200).json({ Helia: idsLocal, Orbit: idsOrbit });
+});
 
-for await (const record of db2.iterator()) {
-  console.log(record);
-}
+router.delete("/del/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    const doc = await db1.get(id);
+    if (doc == undefined) {
+      throw new Error("Document does not exists");
+    }
+    await ipfs1.datastore.delete(new String(doc.value.path.split("/")[2]))
+    await db1.del(id);
+    res.status(200).json({ message: "Document deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-await db1.close()
-await db2.close()
-await orbitdb1.stop()
-await orbitdb2.stop()
-await ipfs1.stop()
-await ipfs2.stop()
+db2.events.on("update", (entry) => {
+  console.log(entry);
+});
+
+// await db1.close();
+// await db2.close();
+// await orbitdb1.stop();
+// await orbitdb2.stop()
+// await ipfs1.stop();
+// await ipfs2.stop()
 
 export default router;
